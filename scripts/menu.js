867155d65d63fe51966120b8306761e39d1017c9
rysmartinez <rysmartinez7@gmail.com>
2025-03-08 18:43:24 -0600

    resposive layout updated

--- scripts/menu.js
+++ scripts/menu.js
@@ -1,15 +1,4 @@
-// Mobile menu toggle functionality
-//document.addEventListener("DOMContentLoaded", function () {
-//const menuToggle = document.getElementById("menu-toggle");
-//const menu = document.getElementById("menu");
-
-//if (menuToggle && menu) {
-//menuToggle.addEventListener("click", function () {
-//menu.classList.toggle("show");
-//});
-//}
-//});
-
+//
 const hamButton = document.querySelector('#menu-toggle');
 const navigation = document.querySelector('#menu');
 
