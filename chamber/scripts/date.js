e57febd6d184ff12b9ebc58a4d35f4bce4aa1c08
rysmartinez <rysmartinez7@gmail.com>
2025-03-11 21:06:25 -0500

    week2 chamber project

---
+++ chamber/scripts/date.js
@@ -0,0 +1,9 @@
+//
+
+var date = new Date();
+var year = date.getFullYear();
+document.getElementById('currentyear').innerHTML = year;
+
+
+
+document.getElementById('lastdatemodified').innerHTML = document.lastModified;
---
+++ chamber/scripts/menu.js
@@ -0,0 +1,8 @@
+//
+const hamButton = document.querySelector('#menu-toggle');
+const navigation = document.querySelector('#menu');
+
+hamButton.addEventListener('click', () => {
+    navigation.classList.toggle('show');
+    hamButton.classList.toggle('show');
+});
